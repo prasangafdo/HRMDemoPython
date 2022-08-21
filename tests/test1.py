f5531611af7b556355d89b1510141103f2de02c6
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-21 18:15:13 +0530

    Fixed browser not opening issue

--- tests/test1.py
+++ tests/test1.py
@@ -1,8 +1,12 @@
 import pytest
-from page import loginpage
+import softest
+from page.loginpage import LoginPage
 
 
 # Test should either start with test or end with test. Same applicable for python file
 
-def test_user_can_login_with_valid_credentials():
-    loginpage.load_login_page()
+class testaaai1(softest.TestCase):
+
+    def test_user_can_login_with_valid_credentials(self):
+        login = LoginPage()
+        login.load_login_page()
