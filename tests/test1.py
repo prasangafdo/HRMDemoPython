fc751a3844ce5193b428d2620ebf548aff511e63
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-21 18:18:24 +0530

    Removed softest

--- tests/test1.py
+++ tests/test1.py
@@ -5,7 +5,7 @@ from page.loginpage import LoginPage
 
 # Test should either start with test or end with test. Same applicable for python file
 
-class testaaai1(softest.TestCase):
+class testaaai1():
 
     def test_user_can_login_with_valid_credentials(self):
         login = LoginPage()
