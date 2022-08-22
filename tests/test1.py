d43320b5667e0024676dad28f6762b88b5f862b3
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-22 10:37:20 +0530

    Updated script to login to the system

--- tests/test1.py
+++ tests/test1.py
@@ -5,8 +5,9 @@ from page.loginpage import LoginPage
 
 # Test should either start with test or end with test. Same applicable for python file
 
-class testaaai1():
+class TestRunner:
 
     def test_user_can_login_with_valid_credentials(self):
         login = LoginPage()
         login.load_login_page()
+        login.login_with_valid_credentials()
