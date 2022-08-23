da306f41801b577e98ad6a2a44296d49784403ab
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-23 13:54:38 +0530

    Update loginpage.py
    Added explicit wait

--- page/loginpage.py
+++ page/loginpage.py
@@ -2,6 +2,8 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from resources import constants
 
@@ -19,8 +21,8 @@ class LoginPage:
 
     def login_with_valid_credentials(self):
         # time.sleep(5)
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.visibility_of_element_located((By.NAME, self.lbl_username)))
         self.driver.find_element(By.NAME, self.lbl_username).send_keys(constants.username)
         self.driver.find_element(By.NAME, self.lbl_password).send_keys(constants.password)
         self.driver.find_element(By.XPATH, self.btn_login).click()
-
-
