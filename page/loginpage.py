0095b46564da4fe06047ecda4e605ddaed721264
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-24 20:35:39 +0530

    Removed class declaration

--- page/loginpage.py
+++ page/loginpage.py
@@ -7,22 +7,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from resources import constants
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-class LoginPage:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+lbl_username = "username"
+lbl_password = "password"
+btn_login = "//button[normalize-space()='Login']"
 
-    lbl_username = "username"
-    lbl_password = "password"
-    btn_login = "//button[normalize-space()='Login']"
 
-    def load_login_page(self):
-        self.driver.set_page_load_timeout(10)
-        self.driver.get(constants.baseURL)
+def load_login_page(self):
+    self.driver.set_page_load_timeout(10)
+    self.driver.get(constants.baseURL)
 
-    def login_with_valid_credentials(self):
-        # time.sleep(5)
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_element_located((By.NAME, self.lbl_username)))
-        self.driver.find_element(By.NAME, self.lbl_username).send_keys(constants.username)
-        self.driver.find_element(By.NAME, self.lbl_password).send_keys(constants.password)
-        self.driver.find_element(By.XPATH, self.btn_login).click()
+
+def login_with_valid_credentials(self):
+    # time.sleep(5)
+    WebDriverWait(self.driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.NAME, self.lbl_username)))
+    self.driver.find_element(By.NAME, self.lbl_username).send_keys(constants.username)
+    self.driver.find_element(By.NAME, self.lbl_password).send_keys(constants.password)
+    self.driver.find_element(By.XPATH, self.btn_login).click()
--- tests/test1.py
+++ tests/test1.py
@@ -1,6 +1,6 @@
 import pytest
-import softest
-from page.loginpage import LoginPage
+from page import loginpage
+from page.landingpage import LandingPage
 
 
 # Test should either start with test or end with test. Same applicable for python file
@@ -8,6 +8,5 @@ from page.loginpage import LoginPage
 class TestRunner:
 
     def test_user_can_login_with_valid_credentials(self):
-        login = LoginPage()
-        login.load_login_page()
-        login.login_with_valid_credentials()
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
