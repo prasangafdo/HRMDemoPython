9ba07441f303739305b919cc507dcdb220382ca6
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-25 20:23:31 +0530

    Update landingpage.py
    Added a definition to quite browser

--- page/landingpage.py
+++ page/landingpage.py
@@ -1,7 +1,16 @@
-from page.loginpage import LoginPage
+from selenium.webdriver.common.by import By
+
+from page import loginpage
+
+# class LandingPage(LoginPage):
+img_banner = "//img[@alt='client brand banner']"
+
+arg = loginpage
 
 
-class LandingPage(LoginPage):
+def is_orange_hrm_logo_displaying():
+    return loginpage.driver.find_element(By.XPATH, img_banner).is_displayed()
 
-    def is_orange_hrm_logo_displaying(self):
-        pass
+
+def quite_driver():
+    loginpage.driver.quit()
