f1d906c1f841199d04fc79be322d0a954493ec69
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-27 16:55:27 +0530

    Update landingpage.py

--- page/landingpage.py
+++ page/landingpage.py
@@ -4,12 +4,18 @@ from page import loginpage
 
 # class LandingPage(LoginPage):
 img_banner = "//img[@alt='client brand banner']"
+btn_admin = "//li[@class='oxd-main-menu-item-wrapper']/a[contains(@href,'viewAdminModule')]"
 
-arg = loginpage
+
+# arg = loginpage
 
 
 def is_orange_hrm_logo_displaying():
     return loginpage.driver.find_element(By.XPATH, img_banner).is_displayed()
+
+
+def navigate_to_admin_page():
+    loginpage.driver.find_element(By.XPATH, btn_admin).click()
 
 
 def quite_driver():
