c0c6bd1b0adda1eddaf011d44d84929d8d1dfcda
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-29 14:48:33 +0530

    Update usermanagementpage.py
    Added element locators and completed "are_column_headers_displaying" function

--- page/usermanagementpage.py
+++ page/usermanagementpage.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.by import By
 from page import loginpage
 
 lbl_admin_topic = "//span[@class='oxd-topbar-header-breadcrumb']"
+lbl_table_body = "//div[@class='oxd-table-body']"
 
 
 def is_admin_topic_displaying():
-    loginpage.driver.find_element(By.XPATH, lbl_admin_topic).is_displayed()
+    return loginpage.driver.find_element(By.XPATH, lbl_admin_topic).is_displayed()
 
 
 def are_column_headers_displaying():
-    pass  # Will work on this later after fixing the navigation
+    return len(loginpage.driver.find_element(By.XPATH, lbl_table_body).getText) > 0
+    # pass  # Will work on this later after fixing the navigation
