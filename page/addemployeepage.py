1f68d23e4043eb83b8b32e4e8bb9bd95b6944e67
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-31 15:55:07 +0530

    Added a verification point
    Added a verification point

--- page/addemployeepage.py
+++ page/addemployeepage.py
@@ -1,4 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 from page import loginpage
 
 driver = loginpage.driver
+lbl_add_user_topic = "//h6[normalize-space()='Add User']"
 
+
+def add_a_new_admin_user():
+    pass
+
+
+def is_add_user_topic_displaying():
+    WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, lbl_add_user_topic)))
+    return bool(driver.find_element(By.XPATH, lbl_add_user_topic).is_displayed())
--- tests/test1.py
+++ tests/test1.py
@@ -1,6 +1,6 @@
 import pytest
 from page import loginpage
-from page import landingpage
+from page import landingpage , addemployeepage
 from page import usermanagementpage
 
 
@@ -23,10 +23,22 @@ class TestRunner:
         assert bool(usermanagementpage.are_column_headers_displaying()) == True
         landingpage.quite_driver()
 
-    def test_admin_can_add_an_employee_without_login_details(self):
+    # def test_admin_can_add_an_employee_without_login_details(self):
+    #     loginpage.load_login_page()
+    #     loginpage.login_with_valid_credentials()
+    #     assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+    #     landingpage.navigate_to_admin_page()
+    #     assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+
+    def test_admin_can_add_a_new_system_user(self):
         loginpage.load_login_page()
         loginpage.login_with_valid_credentials()
         assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
         landingpage.navigate_to_admin_page()
         assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+        usermanagementpage.open_add_system_user_page()
+        assert addemployeepage.is_add_user_topic_displaying() == True
+        # completed the navigation to add user page
+        landingpage.quite_driver()
+
 
