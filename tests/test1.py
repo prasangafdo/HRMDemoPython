8f0aa1f72c438acfe61f307ee2153d42d9f275f0
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-31 15:41:40 +0530

    Started working on "test_admin_can_add_an_employee_without_login_details" function

--- tests/test1.py
+++ tests/test1.py
@@ -1,5 +1,3 @@
-from operator import truth
-
 import pytest
 from page import loginpage
 from page import landingpage
@@ -24,3 +22,11 @@ class TestRunner:
         assert bool(usermanagementpage.is_admin_topic_displaying()) == True
         assert bool(usermanagementpage.are_column_headers_displaying()) == True
         landingpage.quite_driver()
+
+    def test_admin_can_add_an_employee_without_login_details(self):
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
+        assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+        landingpage.navigate_to_admin_page()
+        assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+
