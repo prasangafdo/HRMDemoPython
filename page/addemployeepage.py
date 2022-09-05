dfc3aa371994e1cdbc722d394dcb01f444eb7d93
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-05 19:54:34 +0530

    Update addemployeepage.py
    Completed up to selecting admin option from the drop down

--- page/addemployeepage.py
+++ page/addemployeepage.py
@@ -6,13 +6,21 @@ from page import loginpage
 
 driver = loginpage.driver
 lbl_add_user_topic = "//h6[normalize-space()='Add User']"
+drp_dwn_user_role = "//div[normalize-space()='User Role']//ancestor::div[@class='oxd-input-group " \
+                "oxd-input-field-bottom-space']//div[@class='oxd-select-wrapper'] "
+drp_dwn_admin = "//div[@role='listbox']//div[normalize-space()='Admin']"
+
+btn_save = "//button[normalize-space()='Save']"
 
 
 def add_a_new_admin_user():
-    pass
+    driver.find_element(By.XPATH, drp_dwn_user_role).click()
+    driver.find_element(By.XPATH, drp_dwn_admin).click()
+    driver.find_element(By.XPATH, btn_save).click()
 
 
 def is_add_user_topic_displaying():
     WebDriverWait(driver, 10).until(
         expected_conditions.visibility_of_element_located((By.XPATH, lbl_add_user_topic)))
     return bool(driver.find_element(By.XPATH, lbl_add_user_topic).is_displayed())
+# //div[@class='oxd-select-wrapper']
