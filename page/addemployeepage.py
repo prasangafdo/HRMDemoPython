93dd5d94ddde2d0daeb2c8c9583fc5e210b1bd6c
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-07 17:13:28 +0530

    Update addemployeepage.py

--- page/addemployeepage.py
+++ page/addemployeepage.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -9,13 +11,17 @@ lbl_add_user_topic = "//h6[normalize-space()='Add User']"
 drp_dwn_user_role = "//div[normalize-space()='User Role']//ancestor::div[@class='oxd-input-group " \
                 "oxd-input-field-bottom-space']//div[@class='oxd-select-wrapper'] "
 drp_dwn_admin = "//div[@role='listbox']//div[normalize-space()='Admin']"
-
 btn_save = "//button[normalize-space()='Save']"
+txt_employee_name = "//input[@placeholder='Type for hints...']"
+drp_dwn_employee_name = "//div[contains(@class,'oxd-autocomplete-dropdown')]"
 
 
 def add_a_new_admin_user():
     driver.find_element(By.XPATH, drp_dwn_user_role).click()
     driver.find_element(By.XPATH, drp_dwn_admin).click()
+    driver.find_element(By.XPATH, txt_employee_name).send_keys("arjun")
+    time.sleep(3)
+    driver.find_element(By.XPATH, drp_dwn_employee_name).click()
     driver.find_element(By.XPATH, btn_save).click()
 
 
