68f76041bd7b182b8067dc04a40846f5c8c06714
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-13 19:04:24 +0530

    Completed "search_by_username" function

--- tests/test1.py
+++ tests/test1.py
@@ -40,11 +40,30 @@ class TestRunner:
         assert addemployeepage.is_add_user_topic_displaying() == True
         addemployeepage.add_a_new_admin_user()
         assert addemployeepage.is_save_success_message_displaying() == True
-        # usermanagementpage.open_add_system_user_page()
-        # assert addemployeepage.is_add_user_topic_displaying() == True
-        # addemployeepage.add_a_new_admin_user()
-        # assert addemployeepage.is_save_success_message_displaying() == True
+        usermanagementpage.open_add_system_user_page()
+        assert addemployeepage.is_add_user_topic_displaying() == True
+        addemployeepage.add_a_new_admin_user()
+        assert addemployeepage.is_save_success_message_displaying() == True
         landingpage.quite_driver()
+
+    def test_admin_can_search_a_system_user_by_username(self):
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
+        assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+        landingpage.navigate_to_admin_page()
+        assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+        username = "Fiona.Grace"
+        assert username in usermanagementpage.search_by_username(username)
+        landingpage.quite_driver()
+
+
+    # def test_admin_can_delete_a_system_user(self):
+    #     loginpage.load_login_page()
+    #     loginpage.login_with_valid_credentials()
+    #     assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+    #     landingpage.navigate_to_admin_page()
+    #     assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+
 
 
 
