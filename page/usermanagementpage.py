3f65b25af22ebe9033e7c18b006a7e5b389863d9
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-13 19:04:29 +0530

    Completed "search_by_username" function

--- page/usermanagementpage.py
+++ page/usermanagementpage.py
@@ -7,6 +7,9 @@ from page import loginpage
 lbl_admin_topic = "//span[@class='oxd-topbar-header-breadcrumb']"
 lbl_table_body = "//div[@class='oxd-table-body']"
 btn_add_system_user = "//button[normalize-space()='Add']"
+txt_username = "//div[normalize-space()='Username']//input"
+btn_search = "//button[normalize-space()='Search']"
+lbl_table_rows = "//div[@class='oxd-table-card']//div[contains(@class,'oxd-table-row')]"
 driver = loginpage.driver
 
 
@@ -24,3 +27,11 @@ def are_column_headers_displaying():
 
 def open_add_system_user_page():
     driver.find_element(By.XPATH, btn_add_system_user).click()
+
+
+def search_by_username(username):
+    driver.find_element(By.XPATH, txt_username).send_keys(username)
+    driver.find_element(By.XPATH, btn_search).click()
+    WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, lbl_table_rows)))
+    return str(driver.find_element(By.XPATH, lbl_table_rows).text)
