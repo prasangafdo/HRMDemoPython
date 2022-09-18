63e1e645bd683d83c070285f68152acbac12e6bb
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-18 15:26:53 +0530

    Update edituserpage.py

--- page/edituserpage.py
+++ page/edituserpage.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from page import loginpage
+
+driver = loginpage.driver
+
 btn_edit_user = "//i[@class='oxd-icon bi-pencil-fill']/parent::button"
+
+
+def is_edit_icon_displaying():
+    WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, btn_edit_user)))
+    return driver.find_element(By.XPATH, btn_edit_user).is_displayed()
