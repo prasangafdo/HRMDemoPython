b0132a45b9a2ef6d9b11111be2ab0d0ac418abd4
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-28 19:52:24 +0530

    Update job_test.py

---
+++ tests/job_test.py
@@ -0,0 +1,16 @@
+import pytest
+from page import jobtitlespage as job_title
+from page import loginpage
+from page import landingpage, addemployeepage, edituserpage
+from page import usermanagementpage
+
+
+class TestJobs:
+
+    def test_admin_can_view_existing_job_titles(self):
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
+        assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+        landingpage.navigate_to_admin_page()
+        assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+        assert job_title.is_job_titles_topic_displaying() == True
