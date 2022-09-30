c799960d735f3bb9079a7a494411fd0ad8e29c5d
Prasanga Fernando <prasangafdz@outlook.com>
2022-09-30 18:38:52 +0530

    Fixed xpaths and updated navigation to Jobs

--- page/landingpage.py
+++ page/landingpage.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -7,7 +8,8 @@ from page import loginpage
 # class LandingPage(LoginPage):
 img_banner = "//img[@alt='client brand banner']"
 btn_admin = "//li[@class='oxd-main-menu-item-wrapper']/a[contains(@href,'viewAdminModule')]"
-
+btn_job = "//li[normalize-space()='Job']"
+lbl_job_titles = "//a[normalize-space()='Job Titles']"
 
 # arg = loginpage
 
@@ -20,6 +22,12 @@ def is_orange_hrm_logo_displaying():
 
 def navigate_to_admin_page():
     loginpage.driver.find_element(By.XPATH, btn_admin).click()
+
+
+def navigate_to_job_titles():
+    loginpage.driver.find_element(By.XPATH, btn_job).click()
+    action = ActionChains(loginpage.driver)
+    action.move_to_element(loginpage.driver.find_element(By.XPATH, lbl_job_titles)).click().perform()
 
 
 def quite_driver():
