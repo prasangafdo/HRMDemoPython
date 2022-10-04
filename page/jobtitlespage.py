479ec4deb065676c3cadc88ac7c83180db352a9f
Prasanga Fernando <prasangafdz@outlook.com>
2022-10-04 14:37:26 +0530

    Update jobtitlespage.py

--- page/jobtitlespage.py
+++ page/jobtitlespage.py
@@ -9,9 +9,16 @@ from page import loginpage
 driver = loginpage.driver
 
 lbl_job_titles_topic = "//h6[text()='Job Titles']"
+lbl_table_rows = "//div[@class='oxd-table-card']//div[contains(@class,'oxd-table-row')]"
 
 
 def is_job_titles_topic_displaying():
     WebDriverWait(driver, 10).until(
         expected_conditions.visibility_of_element_located((By.XPATH, lbl_job_titles_topic)))
     return driver.find_element(By.XPATH, lbl_job_titles_topic).is_displayed()
+
+
+def are_table_data_not_empty():
+    WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, lbl_table_rows)))
+    return bool(len(driver.find_element(By.XPATH, lbl_table_rows).text)>0)
