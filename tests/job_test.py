a9fa7485b1334d3a468cd32c21bbde06e1f72397
Prasanga Fernando <prasangafdz@outlook.com>
2022-10-07 15:35:41 +0530

    Update job_test.py

--- tests/job_test.py
+++ tests/job_test.py
@@ -16,3 +16,17 @@ class TestJobs:
         # assert bool(usermanagementpage.is_admin_topic_displaying()) == True
         assert job_title.is_job_titles_topic_displaying() == True
         assert job_title.are_table_data_not_empty() == True
+        landingpage.quite_driver()
+
+    def test_admin_can_add_a_new_job_title(self):
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
+        assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+        landingpage.navigate_to_admin_page()
+        landingpage.navigate_to_job_titles()
+        # assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+        assert job_title.is_job_titles_topic_displaying() == True
+        assert job_title.are_table_data_not_empty() == True
+        job_title.click_on_add_job_title_button()
+
+        # landingpage.quite_driver()
