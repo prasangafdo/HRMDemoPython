d8d11d9be6664cbe1830a22ce87481eb36b1400d
Prasanga Fernando <prasangafdz@outlook.com>
2022-10-18 19:17:06 +0530

    Update job_test.py
    Started working on test_admin_can_edit_job_title function

--- tests/job_test.py
+++ tests/job_test.py
@@ -31,3 +31,17 @@ class TestJobs:
         job_title.enter_job_title()
         assert job_title.is_save_success_message_displaying() == True
         landingpage.quite_driver()
+
+    def test_admin_can_edit_job_title(self):
+        loginpage.load_login_page()
+        loginpage.login_with_valid_credentials()
+        assert bool(landingpage.is_orange_hrm_logo_displaying()) == True
+        landingpage.navigate_to_admin_page()
+        landingpage.navigate_to_job_titles()
+        # assert bool(usermanagementpage.is_admin_topic_displaying()) == True
+        assert job_title.is_job_titles_topic_displaying() == True
+        assert job_title.are_table_data_not_empty() == True
+        # job_title.click_on_add_job_title_button()
+        # job_title.enter_job_title()
+        # assert job_title.is_save_success_message_displaying() == True
+        # landingpage.quite_driver()
